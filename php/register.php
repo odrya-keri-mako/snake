f45e9f85aef048828e2ccec5a27403b2322ee8e3
kardos-zoltan <kardos.zoltan-2021@keri.mako.hu>
2025-10-01 08:21:09 +0200

    regisztráció

--- php/register.php
+++ php/register.php
@@ -1,3 +1,24 @@
 <?php
 require_once("../components/php/environment.php");
 
+$args = Util::getArgs();
+
+if (!is_string($args["name"]) || mb_strlen($args["name"]) > 50)
+    Util::setError("Nem helyes a név!");
+if (!is_int($args["score"]))
+    Util::setError("A scorenak intnek kell lennie!");
+
+$db = new Database();
+
+$query = "
+    INSERT INTO `user` (`name`, `score`)
+    VALUES (:name, :score)
+";
+
+$result = $db->execute($query, $args);
+
+if (!$result["affectedRows"])
+    Util::setError("Sikertelen adatfelvitel");
+
+Util::setResponse();
+
